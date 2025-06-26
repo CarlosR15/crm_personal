fc133bf84497f7982ed75e15a16ddbd13e7d6d7f
Carlos Romellon <crromellon@outlook.com>
2025-06-25 22:36:42 -0600

    redux corregido, no sirve
    no guarda individualmente, me imagino que efectivamente hay que poner asyncstorage
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- App.tsx
+++ App.tsx
@@ -2,16 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'expo-router/entry';
 import RootLayout from './app/_layout';
-import { store } from './src/core/store';
-import { Provider } from 'react-redux';
+
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <RootLayout />
-      </View>
-    </Provider>
+    <View style={styles.container}>
+      <RootLayout />
+    </View>
   );
 }
 
